1143407eaf3c2ea02e19d0e6ed8943885af3ef59
Grant Vinson <vinsongrant@gmail.com>
2021-07-16 11:04:49 -0500

    Updating tabs and settings

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/scss/app.scss', 'public/css')
-    .version();
+	.sass('resources/scss/app.scss', 'public/css')
+	.version();
